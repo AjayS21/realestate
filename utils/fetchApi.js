a35a88475d398be55941cc525e2a0c3c4debc770
Ajay Shah <72499157+AjayS21@users.noreply.github.com>
2022-09-10 15:32:00 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': 'ce9957c843msh3ad575701fe2e7ep1b682bjsn639aa9fe5f97' ,
+      'x-rapidapi-key': '<YOUR RAPID API KEY' ,
     },
   });
     
