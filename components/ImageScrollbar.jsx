76c45ffa39b4b08b71bc61dcf7a2c313f9c0eecf
Ajay Shah <72499157+AjayS21@users.noreply.github.com>
2022-09-10 11:37:25 +0530

    Update ImageScrollbar.jsx

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -6,7 +6,6 @@ import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
-  key={null};
 
   return (
     <Flex justifyContent='center' alignItems='center' marginRight='1'>
