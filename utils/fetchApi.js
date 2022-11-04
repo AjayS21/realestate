2d39e50391c0b0e83f5e63adac6ec882735f896d
Ajay Shah <72499157+AjayS21@users.noreply.github.com>
2022-11-04 21:50:30 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -15,6 +15,6 @@ export const fetchApi = async (url) => {
 
 
 //  headers: {
-//    'X-RapidAPI-Key': 'ce9957c843msh3ad575701fe2e7ep1b682bjsn639aa9fe5f97',
+//    'X-RapidAPI-Key': '',
 //    'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
 //  }
